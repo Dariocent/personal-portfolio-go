ceefb7659aa37e399b2ad424f67cb530ef513fcf
Dario Cantella <dcantella@2chc5l.onmicrosoft.com>
2024-04-03 14:40:32 +0200

    Simple static webserver in Go/Echo

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+
+    "github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+    e := echo.New()
+
+    e.GET("/", func(c echo.Context) error {
+		return c.File("templates/index.html")
+	})
+
+	e.Static("/static", "static")
+
+    e.Start(":8080")
 }
